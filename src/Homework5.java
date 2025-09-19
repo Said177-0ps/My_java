d9ace93ca7dec37efaecd178fbdb7fab1431a1a3
Said177-0ps <guliyevsaid717@gmail.com>
2025-09-19 17:17:06 +0400

    My_java
    my homework 2 attempt

---
+++ src/Homework5.java
@@ -0,0 +1,28 @@
+public class Homework5 {
+    public static void main(String[] args) {
+       int[] mymassive = {34, 65, 43, 12, 155, 178, 134, 67, 80, 100};
+         //незнаю как сделать но знаю какое операция <
+
+        String[] dayofweeks = {"monday", "tuesday", "wendsday", "thursday", "friday", "saturday", "sunday"};
+
+        System.out.println("Дни Недели:");
+        System.out.println(dayofweeks[0]);
+        System.out.println(dayofweeks[1]);
+        System.out.println(dayofweeks[2]);
+        System.out.println(dayofweeks[3]);
+        System.out.println(dayofweeks[4]);
+        System.out.println(dayofweeks[5]);
+        System.out.println(dayofweeks[6]);
+
+        System.out.println("Нечётные индексы:");
+        System.out.println(dayofweeks[1]);
+        System.out.println(dayofweeks[3]);
+        System.out.println(dayofweeks[5]);
+
+        System.out.println("Чётные индексы:");
+        System.out.println(dayofweeks[0]);
+        System.out.println(dayofweeks[2]);
+        System.out.println(dayofweeks[4]);
+        System.out.println(dayofweeks[6]);
+        }
+    }
---
+++ src/homework6.java
@@ -0,0 +1,13 @@
+public class homework6 {
+    public static void main(String[] args) {
+        int a = 312;
+        int b = 79;
+
+        System.out.println("Сумма а и б: " + (a + b));
+        System.out.println("Разность а и б: " + (a - b));
+        System.out.println("Произведение а и б: " + (a * b));
+        System.out.println("Целое деление а и б: " + (a / b));
+        System.out.println("Остаток от деления а и б: " + (a % b));
+    }
+}
+
