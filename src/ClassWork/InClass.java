5b76c522cf7363b71f21524e20c483964bcb4fc0
Said177-0ps <guliyevsaid717@gmail.com>
2025-10-22 21:15:21 +0400

    вот

--- src/ClassWork/InClass.java
+++ src/ClassWork/InClass.java
@@ -1,14 +1,57 @@
 package ClassWork;
 
-import java.util.Scanner;
-
-
 public class InClass {
     public static void main(String[] args) {
-        InClassMethods cao = new InClassMethods();
-        Scanner scanner = new Scanner(System.in);
+        People poe = new People();
+        poe.setHeight("143");
+        System.out.println(poe.getHeight());
 
-        cao.AboutCar();
-        cao.Int();
+        poe.setBalance("14Azn");
+        System.out.println(poe.getBalance());
+
+        poe.setSurname("Guliyev");
+        System.out.println(poe.getSurname());
+
+        poe.setFavoriteColor("Dark");
+        System.out.println(poe.getFavoriteColor());
+    }
+}
+
+class People{
+    private String height;
+    private String Balance;
+    private String surname;
+    private String FavoriteColor;
+
+    public String getHeight(){
+        return height;
+    }
+
+    public String getBalance(){
+        return Balance;
+    }
+
+    public String getSurname(){
+        return surname;
+    }
+
+    public String getFavoriteColor(){
+        return FavoriteColor;
+    }
+
+    public void setHeight(String height){
+        this.height = height;
+    }
+
+    public void setBalance(String Balance){
+        this.Balance = Balance;
+    }
+
+    public void setSurname(String surname){
+        this.surname = surname;
+    }
+
+    public void setFavoriteColor(String FavoriteColor){
+        this.FavoriteColor = FavoriteColor;
     }
 }
